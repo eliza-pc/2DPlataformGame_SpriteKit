6dc4dac348804c22cac5d3b8198190e453caf214
Eliza Carvalho <elizampcarvalho@gmail.com>
2019-05-23 10:41:46 -0400

    Adiciona func AddButtons Adiciona func setUpControls

--- 2DPlataformGame_SpriteKit/Components/TouchControlInputNode.swift
+++ 2DPlataformGame_SpriteKit/Components/TouchControlInputNode.swift
@@ -28,5 +28,23 @@ class TouchControlInputNode: SKSpriteNode{
         super.init(coder: aDecoder)
     }
     
+    func setUpControls(size: CGSize){
+        
+        addButton(button: buttonRight, position: CGPoint(x: -(size.width / 3) + 50  , y: -(size.height)/4 ), name: "right", scale: 2.0)
+        addButton(button: buttonLeft, position: CGPoint(x: -(size.width / 3) - 50  , y: -(size.height)/4 ), name: "left", scale: 2.0)
+        
+        addButton(button: buttonJump, position: CGPoint(x: -(size.width / 3) + 50  , y: -(size.height)/4 ), name: "jump", scale: 0.40)
+    
+    }
+    
+    func addButton(button: SKSpriteNode, position: CGPoint, name: String, scale: CGFloat){
+        button.position = position
+        button.setScale(scale)
+        button.name = name
+        button.zPosition = 10
+        button.alpha = alphaPressed
+        self.addChild(button)
+        
+    }
     
 }
